fb59c3751c098df6a1633d14e2b1a85991532c9e
Won Wook SONG <wsong0512@gmail.com>
2015-06-28 02:25:21 +0900

    add Comments into CommentsList

--- public/scripts/practice.js
+++ public/scripts/practice.js
@@ -2,7 +2,7 @@ var CommentsBox = React.createClass({
 	render: function() {
 		return (
 			<div className="commentsBox">
-				Hello, world! I am a CommentsBox.
+				<h1>Comments</h1>
 				<CommentsList />
 				<CommentsForm />
 			</div>
@@ -11,24 +11,38 @@ var CommentsBox = React.createClass({
 });
 
 var CommentsList = React.createClass({
-  render: function() {
-    return (
-      <div className="commentsList">
-        Hello, world! I am a CommentsList.
-      </div>
-    );
-  }
+  	render: function() {
+    	return (
+      		<div className="commentsList">
+        		<Comments author="Ricky Song">Comments one!</Comments>
+        		<Comments author="Jordan Walke">This is *another* comment!</Comments>
+      		</div>
+    	);
+  	}
 });
 
 var CommentsForm = React.createClass({
-  render: function() {
-    return (
-      <div className="commentsForm">
-        Hello, world! I am a CommentsForm.
-      </div>
-    );
-  }
+  	render: function() {
+    	return (
+      		<div className="commentsForm">
+        		Hello, world! I am a CommentsForm.
+      		</div>
+    	);
+  	}
 });
+
+var Comments = React.createClass({
+	render: function() {
+		return (
+			<div className="comments">
+				<h2 className="commentsAuthor">
+					{this.props.author}
+				</h2>
+				{this.props.children}
+			</div>
+		)
+	}
+})
 
 React.render(
 	<CommentsBox />,
